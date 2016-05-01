73e37de079769ef04bc5df6398a7a3572c887162
Rasaad <rasaad@o2.pl>
2016-05-01 15:52:23 +0200

    Task 2# zrobiony okienka logowania oraz rejestracji

---
+++ Czat/Rejestracja.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Czat
+{
+    /// <summary>
+    /// Interaction logic for Rejestracja.xaml
+    /// </summary>
+    public partial class Rejestracja : Window
+    {
+        public Rejestracja()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginOn_Click(object sender, RoutedEventArgs e)
+        {
+            logowanie log = new logowanie();
+            log.Show();
+            this.Close();
+        }
+
+        private void Rejestracja_Click(object sender, RoutedEventArgs e)
+        {
+            logowanie log = new logowanie();
+            log.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Czat/logowanie.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Czat
+{
+    /// <summary>
+    /// Interaction logic for logowanie.xaml
+    /// </summary>
+    public partial class logowanie : Window
+    {
+        public logowanie()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginOn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow MW = new MainWindow();
+            MW.Show();
+            this.Close();
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            Rejestracja rej = new Rejestracja();
+            rej.Show();
+            this.Close();
+        }
+    }
+}
