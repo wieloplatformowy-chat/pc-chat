0630cf72566cda4988e166c4e6da287fda844740
xylof <mar94bor@gmail.com>
2016-06-02 11:44:14 +0200

    Poprawa logiki dodawania wiadomości do okna oraz dodanie ikony emotikon do projektu

--- Czat/Views/MainWindow.xaml.cs
+++ Czat/Views/MainWindow.xaml.cs
@@ -11,9 +11,23 @@ namespace Czat.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        private const double MergeInterval = 30;
+
+        private FlowDocument document;
+        private DateTime currentTime;
+        private DateTime previousTime;
+        private TimeSpan timeRange;
+        private Paragraph messageParagraph;
+        private Paragraph dateTimeParagraph;
+
         public MainWindow()
         {
             InitializeComponent();
+            document = new FlowDocument();
+            MsgView.Document = document;
         }
 
         private void OnKeyDownHandler(object sender, KeyEventArgs e)
@@ -24,31 +38,84 @@ namespace Czat.Views
             }
         }
 
-        FlowDocument doc = new FlowDocument();
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SendMsg_Click(object sender, RoutedEventArgs e)
         {
-            if ((TextOfMsg.Text != null) && (TextOfMsg.Text.ToString() != ""))
+            if (string.IsNullOrWhiteSpace(TextOfMsg.Text))
+                return;
+
+            AddMessage(TextOfMsg.Text);
+            TextOfMsg.Text = null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="isNewNeeded"></param>
+        /// <returns></returns>
+        private Paragraph GetMessageParagraph(bool isNewNeeded = false)
+        {
+            if (messageParagraph != null && !isNewNeeded)
+                return messageParagraph;
+
+            messageParagraph = new Paragraph();
+            messageParagraph.FontSize = 12;
+            messageParagraph.Background = System.Windows.Media.Brushes.DarkCyan;
+            messageParagraph.Foreground = System.Windows.Media.Brushes.White;
+
+            return messageParagraph;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="isNewNeeded"></param>
+        /// <returns></returns>
+        private Paragraph GetDateTimeParagraph(bool isNewNeeded = false)
+        {
+            if (dateTimeParagraph != null && !isNewNeeded)
+                return dateTimeParagraph;
+
+            dateTimeParagraph = new Paragraph();
+            dateTimeParagraph.FontSize = 8;
+
+            return dateTimeParagraph;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        private void AddMessage(string message)
+        {
+            currentTime = DateTime.Now;
+
+            // Check if new or merged message
+            // TODO sprawdź czy sender jest ten sam
+            bool isNewMessage = true;
+            timeRange = currentTime - previousTime;
+            if (timeRange.TotalSeconds <= MergeInterval)
+                isNewMessage = false;
+
+            var msgParagraph = GetMessageParagraph(isNewMessage);
+            var dateParagraph = GetDateTimeParagraph(isNewMessage);
+
+            if (isNewMessage)
             {
-                DateTime DateNow = DateTime.Now;
-                
-                Paragraph para1 = new Paragraph();
-                Paragraph para2 = new Paragraph();
-
-                para1.Inlines.Add(new Bold(new Run(TextOfMsg.Text)));
-                para1.FontSize = 12;
-                para1.Background = System.Windows.Media.Brushes.DarkCyan;
-                para1.Foreground = System.Windows.Media.Brushes.White;
-                doc.Blocks.Add(para1);
-
-                para2.Inlines.Add(new Run(DateNow.ToString("HH:mm")));
-                para2.FontSize = 8;
-                doc.Blocks.Add(para2);
-
-                MsgView.Document = doc;
-
-                TextOfMsg.Text = null;
+                document.Blocks.Add(messageParagraph);
+                document.Blocks.Add(dateTimeParagraph);
+                dateTimeParagraph.Inlines.Add(new Run(currentTime.ToString("HH:mm")));
             }
+            else
+                message = "\n" + message;
+
+            messageParagraph.Inlines.Add(new Bold(new Run(message)));
+
+            previousTime = currentTime;
         }
     }
 }
