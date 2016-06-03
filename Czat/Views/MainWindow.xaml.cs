1436c804ba4dccd4eeeb1c5574df9b2239d34a1e
xylof <mar94bor@gmail.com>
2016-06-03 22:32:11 +0200

    Wstępne dodanie okienka popup do wybierania emotikonów, dodanie scroll baru do okna wyświetlania wiadomości, sprawdzanie czy ostatnią wiadomość wysłała ta sama osoba i dodanie komentarzy

--- Czat/Views/MainWindow.xaml.cs
+++ Czat/Views/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ namespace Czat.Views
     public partial class MainWindow : Window
     {
         /// <summary>
-        /// 
+        /// Time in seconds after which messages of the same sender are written separately
         /// </summary>
         private const double MergeInterval = 30;
 
@@ -22,6 +22,10 @@ namespace Czat.Views
         private TimeSpan timeRange;
         private Paragraph messageParagraph;
         private Paragraph dateTimeParagraph;
+        private bool toOpen = true;
+        private RestApiService.Model.UserDTO me = new RestApiService.Model.UserDTO();
+        private RestApiService.Model.UserDTO currentSender = new RestApiService.Model.UserDTO();
+        private RestApiService.Model.UserDTO previousSender = new RestApiService.Model.UserDTO();
 
         public MainWindow()
         {
@@ -39,7 +43,7 @@ namespace Czat.Views
         }
 
         /// <summary>
-        /// 
+        /// Send button click event
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -48,34 +52,46 @@ namespace Czat.Views
             if (string.IsNullOrWhiteSpace(TextOfMsg.Text))
                 return;
 
-            AddMessage(TextOfMsg.Text);
+            AddMessage(TextOfMsg.Text, me.Id);
             TextOfMsg.Text = null;
         }
 
         /// <summary>
-        /// 
+        /// Gets message paragraph
         /// </summary>
-        /// <param name="isNewNeeded"></param>
+        /// <param name="isNewNeeded">Checks if new paragraph is needed</param>
+        /// <param name="senderId">Id of current message sender</param>
         /// <returns></returns>
-        private Paragraph GetMessageParagraph(bool isNewNeeded = false)
+        private Paragraph GetMessageParagraph(bool isNewNeeded, long? senderId)
         {
             if (messageParagraph != null && !isNewNeeded)
                 return messageParagraph;
 
             messageParagraph = new Paragraph();
             messageParagraph.FontSize = 12;
-            messageParagraph.Background = System.Windows.Media.Brushes.DarkCyan;
-            messageParagraph.Foreground = System.Windows.Media.Brushes.White;
+
+            if (senderId == me.Id)
+            {
+                messageParagraph.Background = System.Windows.Media.Brushes.DarkCyan;
+                messageParagraph.Foreground = System.Windows.Media.Brushes.White;
+                messageParagraph.TextAlignment = TextAlignment.Right;
+            }
+            else
+            {
+                messageParagraph.Background = System.Windows.Media.Brushes.LightGray;
+                messageParagraph.TextAlignment = TextAlignment.Left;
+            }
 
             return messageParagraph;
         }
 
         /// <summary>
-        /// 
+        /// Gets date time paragraph
         /// </summary>
-        /// <param name="isNewNeeded"></param>
+        /// <param name="isNewNeeded">Checks if new paragraph is needed</param>
+        /// <param name="senderId">Id of current message sender</param>
         /// <returns></returns>
-        private Paragraph GetDateTimeParagraph(bool isNewNeeded = false)
+        private Paragraph GetDateTimeParagraph(bool isNewNeeded, long? senderId)
         {
             if (dateTimeParagraph != null && !isNewNeeded)
                 return dateTimeParagraph;
@@ -83,26 +99,38 @@ namespace Czat.Views
             dateTimeParagraph = new Paragraph();
             dateTimeParagraph.FontSize = 8;
 
+            if (senderId == me.Id)
+                dateTimeParagraph.TextAlignment = TextAlignment.Right;
+            else
+                dateTimeParagraph.TextAlignment = TextAlignment.Left;
+
             return dateTimeParagraph;
         }
 
         /// <summary>
-        /// 
+        /// Adds new message
         /// </summary>
-        /// <param name="message"></param>
-        private void AddMessage(string message)
+        /// <param name="message">contents of message</param>
+        /// <param name="senderId">Id of current message sender</param>
+        private void AddMessage(string message, long? senderId)
         {
             currentTime = DateTime.Now;
+            currentSender.Id = senderId;
 
-            // Check if new or merged message
-            // TODO sprawdź czy sender jest ten sam
+            bool areTheSameSenders = false;
             bool isNewMessage = true;
+
+            if (currentSender.Id == previousSender.Id)
+                areTheSameSenders = true;
+
             timeRange = currentTime - previousTime;
-            if (timeRange.TotalSeconds <= MergeInterval)
+
+            // Checks if new or merged message
+            if (timeRange.TotalSeconds <= MergeInterval && areTheSameSenders) 
                 isNewMessage = false;
 
-            var msgParagraph = GetMessageParagraph(isNewMessage);
-            var dateParagraph = GetDateTimeParagraph(isNewMessage);
+            var msgParagraph = GetMessageParagraph(isNewMessage, currentSender.Id);
+            var dateParagraph = GetDateTimeParagraph(isNewMessage, currentSender.Id);
 
             if (isNewMessage)
             {
@@ -116,6 +144,18 @@ namespace Czat.Views
             messageParagraph.Inlines.Add(new Bold(new Run(message)));
 
             previousTime = currentTime;
+            previousSender = currentSender;
+        }
+
+        /// <summary>
+        /// Opens or closes popup window with emoticons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ChooseEmot_Click(object sender, RoutedEventArgs e)
+        {
+            Popup1.IsOpen = toOpen;
+            toOpen = !toOpen;
         }
     }
 }
