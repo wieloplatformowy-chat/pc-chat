84326f73ed0dbe263afb3a37c108093aac5bac8b
Budda Play <Budda Play>
2016-06-06 22:37:47 +0200

    -przywrocono funkcjonalnosc -poprawiono nazwy kontrolek/opisy

--- Czat/Views/RegisterVM.xaml.cs
+++ Czat/Views/RegisterVM.xaml.cs
@@ -1,12 +1,13 @@
-﻿using System.Windows;
-using System.Windows.Input;
+﻿using System;
+using System.Net.Mail;
+using System.Windows;
 using RestApiService;
 using RestApiService.Services;
 
 namespace Czat.Views
 {
     /// <summary>
-    /// Interaction logic for LoginVM.xaml
+    /// Interaction logic for RegisterVM.xaml
     /// </summary>
     public partial class RegisterVM : Window
     {
@@ -19,112 +20,52 @@ namespace Czat.Views
         }
 
         /// <summary>
-        /// Tries to login entered user on click event
+        /// Switches to login window
         /// </summary>
-        private async void LoginOn_Click(object sender, RoutedEventArgs e)
+        private void LoginOn_Click(object sender, RoutedEventArgs e)
         {
+            IoC.Resolve<LoginVM>().Show();
+            Close();
+        }
+
+        /// <summary>
+        /// Tries to register entered user on click event
+        /// </summary>
+        private async void Rejestracja_Click(object sender, RoutedEventArgs e)
+        {
+            // Email validation
+            bool isEmailValid = false;
             try
             {
-                LoginButton.IsEnabled = false;
-                await UserService.Login(Login.Text, Password.Password);
+                new MailAddress(EMail.Text);
+                isEmailValid = true;
+            }
+            catch (Exception) {/* isEmailValid: false */}
+
+            //TODO proper validation
+            // Simple user data validation
+            if (Login.Text.Length > 32 || Password.Password.Length > 32 || !isEmailValid || Password.Password != RepeatPassword.Password)
+                return;
+
+            // Registration
+            try
+            {
+                RegisterButton.IsEnabled = false;
+                await UserService.Register(EMail.Text, Login.Text, Password.Password);
             }
             catch (ApiException apiException)
             {
-                LoginButton.IsEnabled = true;
-                // Get rid of MessageBox
+                //TODO Get rid of MessageBox
                 MessageBox.Show(apiException.Message, "Wystąpił błąd");
+                RegisterButton.IsEnabled = true;
                 return;
             }
+            //TODO Get rid of MessageBox
+            MessageBox.Show("Zarejestrowano!");
 
-            IoC.Resolve<MainWindow>().Show();
+            // Show login window
+            IoC.Resolve<LoginVM>().Show();
             Close();
-        }
-
-        /// <summary>
-        /// Switches to registration window
-        /// </summary>
-        private void Register_Click(object sender, RoutedEventArgs e)
-        {
-            IoC.Resolve<RegisterVM>().Show();
-            Close();
-        }
-
-        /// <summary>
-        /// Adds drag functionality to window
-        /// </summary>
-        private void LoginVM_OnMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            App.Current.MainWindow.DragMove();
         }
     }
 }
-
-//using System;
-//using System.Net.Mail;
-//using System.Windows;
-//using RestApiService;
-//using RestApiService.Services;
-
-//namespace Czat.Views
-//{
-//    /// <summary>
-//    /// Interaction logic for RegisterVM.xaml
-//    /// </summary>
-//    public partial class RegisterVM : Window
-//    {
-//        public UserRestService UserService { get; }
-
-//        public RegisterVM(UserRestService userService)
-//        {
-//            UserService = userService;
-//            InitializeComponent();
-//        }
-
-//        /// <summary>
-//        /// Switches to login window
-//        /// </summary>
-//        private void LoginOn_Click(object sender, RoutedEventArgs e)
-//        {
-//            IoC.Resolve<LoginVM>().Show();
-//            Close();
-//        }
-
-//        /// <summary>
-//        /// Tries to register entered user on click event
-//        /// </summary>
-//        private async void Rejestracja_Click(object sender, RoutedEventArgs e)
-//        {
-//            // Email validation
-//            bool isEmailValid = false;
-//            try {
-//                new MailAddress(Email.Text);
-//                isEmailValid = true;
-//            } catch (Exception) {/* isEmailValid: false */}
-
-//            //TODO proper validation
-//            // Simple user data validation
-//            if (Login.Text.Length > 32 || Pass.Text.Length > 32 || !isEmailValid || Pass.Text != PassRep.Text)
-//                return;
-
-//            // Registration
-//            try 
-//            {
-//                Register.IsEnabled = false;
-//                await UserService.Register(Email.Text, Login.Text, Pass.Text);
-//            }
-//            catch (ApiException apiException)
-//            {
-//                //TODO Get rid of MessageBox
-//                MessageBox.Show(apiException.Message, "Wystąpił błąd");
-//                Register.IsEnabled = true;
-//                return;
-//            }
-//            //TODO Get rid of MessageBox
-//            MessageBox.Show("Zarejestrowano!");
-
-//            // Show login window
-//            IoC.Resolve<LoginVM>().Show();
-//            Close();
-//        }
-//    }
-//}
