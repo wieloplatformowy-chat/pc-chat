9dc88f5d4fd936a9aad491f6f8d87b97c86160d0
Mateusz Jaworski <mathew.jaworski@gmail.com>
2016-06-09 00:46:22 +0200

    stworzenie okienka listy kontaktow

---
+++ Czat/Views/ContactList.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Czat.Views
+{
+    /// <summary>
+    /// Interaction logic for ContactList.xaml
+    /// </summary>
+    public partial class ContactList : Window
+    {
+        public ContactList()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RestApiService/Model/ConversationsResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiService.Model
+{
+    public class ConversationsResponse
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public IList<UserDTO> Users { get; set; }
+    }
+}
