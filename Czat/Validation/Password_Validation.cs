ffe2e8fb507651275dae4753a8c29b718848eecc
Monika Kujawa <moniska_7@wp.pl>
2016-06-13 15:16:32 +0200

    Początek walidacji Logowanie przy użyciu entera

---
+++ Czat/Validation/Password_Validation.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace MaterialDesignColors.WpfExample
+{
+    public class Login_Validation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return string.IsNullOrWhiteSpace((value ?? "").ToString())
+                ? new ValidationResult(false, "Złe hasło.")
+                : ValidationResult.ValidResult;
+        }
+    }
+}
